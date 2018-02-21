156c8e4f9a50b10b5d0f96003362bf372011a0fd
fyAgent <1791836545@qq.com>
2018-02-21 22:47:36 +0800

    开始施工

---
+++ egg-learn-socket/app/router.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @param {Egg.Application} app - egg application
+ */
+module.exports = app => {
+  const { router, controller,io } = app;
+  router.redirect("/","/public/index.html");
+  io.of('/example').route('sendMsg', io.controller.nsp.exchange);
+  
+};
---
+++ egg-learn-socket/config/config.default.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1518614221862_2409';
+
+  // add your config here
+  config.middleware = [];
+  
+  config.io={
+    init:{
+
+    },
+    namespace:{
+    
+      '/':{
+        connectionMiddleware: ['auth'],
+        packetMiddleware:["packet"]
+      },
+      "/example":{
+        connectionMiddleware:["connection"],
+        packetMiddleware:[]
+      }
+
+    }
+  }
+  return config;
+};
